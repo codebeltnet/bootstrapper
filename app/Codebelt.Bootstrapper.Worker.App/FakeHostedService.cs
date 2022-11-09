d089fb0df05bea02c7877f151b176983f2649682
gimlichael <michael@geekle.io>
2022-11-09 22:26:39 +0100

    Improved test app with graceful simulation

--- app/Codebelt.Bootstrapper.Worker.App/FakeHostedService.cs
+++ app/Codebelt.Bootstrapper.Worker.App/FakeHostedService.cs
@@ -9,18 +9,30 @@ namespace Codebelt.Bootstrapper.Worker.App
     public class FakeHostedService : BackgroundService
     {
         private readonly ILogger<FakeHostedService> _logger;
+        private bool _gracefulShutdown = false;
 
         public FakeHostedService(ILogger<FakeHostedService> logger)
         {
             _logger = logger;
+
+            BootstrapperLifetime.OnApplicationStartedCallback = () => logger.LogInformation("Started");
+            BootstrapperLifetime.OnApplicationStoppingCallback = () =>
+            {
+                _gracefulShutdown = true;
+                logger.LogWarning("Stopping and cleaning ..");
+                Thread.Sleep(TimeSpan.FromSeconds(5)); // simulate graceful shutdown
+                logger.LogWarning(".. done!");
+            };
+            BootstrapperLifetime.OnApplicationStoppedCallback = () => logger.LogCritical("Stopped");
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
+                if (_gracefulShutdown) { return; }
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.UtcNow.ToString("O"));
-                await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
+                await Task.Delay(TimeSpan.FromSeconds(2), stoppingToken);
             }
         }
     }
