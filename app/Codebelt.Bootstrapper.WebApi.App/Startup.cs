2ab2f99f00c9764daa241a253efc47a3ed0db652
Michael Mortensen <michael@geekle.io>
2025-11-13 23:36:21 +0100

    :heavy_plus_sign: added support for net10.0 TFM :heavy_minus_sign: remove support for net8.0 TFM

--- app/Codebelt.Bootstrapper.WebApi.App/Startup.cs
+++ app/Codebelt.Bootstrapper.WebApi.App/Startup.cs
@@ -4,8 +4,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using Cuemon.Extensions.Hosting;
+using Microsoft.OpenApi;
 
 namespace Codebelt.Bootstrapper.WebApi.App
 {
