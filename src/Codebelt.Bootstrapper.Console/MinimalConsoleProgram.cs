2055d73875fc8b8015c71a6c52108bee49c925a0
Michael Mortensen <michael@geekle.io>
2025-12-18 00:04:10 +0100

    V5.0.2/service update (#21)
    📦️ updated NuGet package definition
    👷 update ci pipeline name and expand test matrix os
    🐛 refactor MinimalConsoleProgram and add generic variant
    💬 updated community health pages
    ⬆️ bump dependencies

--- src/Codebelt.Bootstrapper.Console/MinimalConsoleProgram.cs
+++ src/Codebelt.Bootstrapper.Console/MinimalConsoleProgram.cs
@@ -15,20 +15,38 @@ namespace Codebelt.Bootstrapper.Console
         /// </summary>
         /// <param name="args">The command line arguments.</param>
         /// <returns>The initialized <see cref="HostApplicationBuilder"/>.</returns>
-        protected static HostApplicationBuilder CreateHostBuilder(string[] args)
-        {
-            var hb = Host.CreateApplicationBuilder(args);
-            hb.UseBootstrapperLifetime();
-            hb.UseBootstrapperProgram(typeof(MinimalConsoleProgram));
-            hb.UseMinimalConsoleProgram();
-            return hb;
-        }
+        protected static HostApplicationBuilder CreateHostBuilder(string[] args) => CreateHostBuilder(args, typeof(MinimalConsoleProgram));
 
         /// <summary>
         /// A convenient method for executing fire-and-forget code.
         /// </summary>
         /// <param name="serviceProvider">The <see cref="IServiceProvider"/> to retrieve services from.</param>
         /// <param name="cancellationToken">Indicates that the run has been aborted.</param>
+        /// <returns>A <see cref="Task"/> that completes when the execution is finished.</returns>
         public abstract Task RunAsync(IServiceProvider serviceProvider, CancellationToken cancellationToken);
+
+        internal static HostApplicationBuilder CreateHostBuilder(string[] args, Type programType)
+        {
+            var hb = Host.CreateApplicationBuilder(args);
+            hb.UseBootstrapperLifetime();
+            hb.UseBootstrapperProgram(programType);
+            hb.UseMinimalConsoleProgram();
+            return hb;
+        }
+    }
+
+    /// <summary>
+    /// The base entry point of an application optimized for console applications with a specific bootstrap type.
+    /// </summary>
+    /// <typeparam name ="TProgram">The type responsible for the application bootstrap.</typeparam>
+    /// <seealso cref="ProgramRoot" />
+    public abstract class MinimalConsoleProgram<TProgram> : MinimalConsoleProgram where TProgram : ProgramRoot
+    {
+        /// <summary>
+        /// Creates an <see cref="HostApplicationBuilder"/> used to set up the host.
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
+        /// <returns>The initialized <see cref="HostApplicationBuilder"/>.</returns>
+        protected new static HostApplicationBuilder CreateHostBuilder(string[] args) => CreateHostBuilder(args, typeof(TProgram));
     }
 }
